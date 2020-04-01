319e98fc9ccd92275577b58262e7b4d6905c345c
Juan Andrés Rueda <juani.rueda@hotmail.com>
2020-04-01 11:34:09 -0300

    Terminados endpoints restantes: post, put y delete

--- Controllers/GastoController.cs
+++ Controllers/GastoController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using AdminGastos.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdminGastos.Controllers
 {
@@ -19,18 +20,62 @@ namespace AdminGastos.Controllers
             return _context.Gastos;
         }
 
-        //GET: api/Gasto/n
+        //GET: api/gasto/n
         [HttpGet("{id}")]
         public ActionResult<Gasto> GetGastoById(int id)
         {
             var gasto = _context.Gastos.Find(id);
 
-            if(gasto == null)
+            if (gasto == null)
             {
                 return NotFound();
             }
 
             return gasto;
         }
+
+        //POST: api/gasto
+
+        [HttpPost]
+        public ActionResult<Gasto> PostGasto(Gasto gasto)
+        {
+            _context.Gastos.Add(gasto);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetGastoById", new Gasto { ID = gasto.ID }, gasto);
+        }
+
+        //PUT: api/gasto/n
+        [HttpPut("{id}")]
+        public ActionResult PutGasto(int id, Gasto gasto)
+        {
+            if (id != gasto.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(gasto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        //DELETE: api/gasto/n
+        [HttpDelete("{id}")]
+        public ActionResult<Gasto> DeleteGasto(int id)
+        {
+            var gasto = _context.Gastos.Find(id);
+
+            if (gasto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Gastos.Remove(gasto);
+            _context.SaveChanges();
+
+            return gasto;
+        }
+
     }
 }
