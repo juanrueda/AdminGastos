ac8656e6e5ec2de43c73e97174c9bf7e22bea6d3
Juan Andrés Rueda <juani.rueda@hotmail.com>
2020-03-31 22:25:57 -0300

    Creado controlador, endpoint get y get por id

--- Controllers/GastoController.cs
+++ Controllers/GastoController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using AdminGastos.Models;
 
 namespace AdminGastos.Controllers
 {
@@ -7,10 +8,29 @@ namespace AdminGastos.Controllers
     [ApiController]
     public class GastoController : ControllerBase
     {
+        private readonly GastoContext _context;
+
+        public GastoController(GastoContext context) => _context = context;
+
+        //GET: api/Gasto
         [HttpGet]
-        public ActionResult<IEnumerable<string>> GetString()
+        public ActionResult<IEnumerable<Gasto>> GetGastos()
         {
-            return new string[] {"this", "is", "hard", "coded"};
+            return _context.Gastos;
+        }
+
+        //GET: api/Gasto/n
+        [HttpGet("{id}")]
+        public ActionResult<Gasto> GetGastoById(int id)
+        {
+            var gasto = _context.Gastos.Find(id);
+
+            if(gasto == null)
+            {
+                return NotFound();
+            }
+
+            return gasto;
         }
     }
 }
